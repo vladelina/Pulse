f0529195adb220e6196e11155c7d55bfd4d59dfd
Vladelina <vlada.zaychenko@gmail.com>
2022-02-06 15:44:07 +0200

    sass

--- gulpfile.js
+++ gulpfile.js
@@ -4,6 +4,7 @@ const sass        = require('gulp-sass')(require('sass'));
 const cleanCSS = require('gulp-clean-css');
 const autoprefixer = require('gulp-autoprefixer');
 const rename = require("gulp-rename");
+const imagemin = require('gulp-imagemin');
 
 gulp.task('server', function() {
 
